f28053bd40f30c113cb795bdd14d0120b1840a9d
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-09 16:16:08 -0700

    Create React App
    Started Project, created nav bar and home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+const Home = () => {
+  return (
+    <div
+    name="home"
+    className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+  >
+    <div className="max-w-screen-lg mx-auto flex flex-col py-0 items-center justify-center h-full lg:h-full px-4 md:flex-row">
+      <div className="flex flex-col justify-center h-full max-h-96">
+        <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          I'm a Software Developer
+        </h2>
+        <p className="text-gray-500 py-4 max-w-md">
+            I am a Junior at Arizona State University pursuing a Bachelors of Computer Science. I love all kinds of programming but I have really fallen in love with web development. 
+            I am profeccient in Java. JavaScript, C++, React.JS ,TailWindCSS and Node.JS. I have experience with C, Scheme, Prolog, MATLAB and more. 
+
+        </p>
+
+        <div>
+          <Link
+            to="portfolio"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-600 to-[#5525c6] cursor-pointer"
+          >
+            Portfolio
+            <span className="group-hover:rotate-90 duration-300">
+              <MdArrowRight size={25} className="ml-1" />
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className = "">
+        <img
+          src={HeroImage}
+          alt="my profile"
+          className="rounded-2xl mx-auto w-2/3 md:w-full"
+        />
+      </div>
+    </div>
+  </div>
+);
+};
+
+export default Home;
