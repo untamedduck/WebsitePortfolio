d3659280e5c177e166f7f881fe036de1f3537d38
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-09 16:16:08 -0700

    Create React App
    Started Project, created nav bar and home page

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import NavBar from "./components/NavBar";
+function App() {
+  return (
+  <div>
+   <NavBar/>
+   <Home/>
+   
+   <SocialLinks/>
+  </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/SocialLinks.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+const SocialLinks = () => {
+
+const links =  [
+    {
+        id:1,
+        child:(
+            <>
+            LinkedIn
+            <FaLinkedin size={30} />
+          </>
+        ),
+        href:"https://www.linkedin.com/in/jaredlee12345/",
+        style:'rounded-tr-md'
+    },
+    {
+        id:2,
+        child:(
+            <>
+            GitHub
+            <FaLinkedin size={30} />
+          </>
+        ),
+        href:"https://www.linkedin.com/in/jaredlee12345/",
+        style:'rounded-tr-md'
+    },
+    {
+        id:3,
+        child:(
+            <>
+            LinkedIn
+            <FaLinkedin size={30} />
+          </>
+        ),
+        href:"https://www.linkedin.com/in/jaredlee12345/",
+        style:'rounded-tr-md'
+    },
+    {
+        id:4,
+        child:(
+            <>
+            LinkedIn
+            <FaLinkedin size={30} />
+          </>
+        ),
+        href:"https://www.linkedin.com/in/jaredlee12345/",
+        style:'rounded-tr-md'
+    },
+]
+
+  return (
+    <div className="flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500 hover:rounded-md duration-300 hover:ml-[-10px]">
+          <a
+            href=""
+            className="flex justify-between items-center w-full text-white"
+          >
+            <>
+              LinkedIn
+              <FaLinkedin size={30} />
+            </>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
