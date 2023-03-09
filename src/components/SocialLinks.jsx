9d708df909f2cb74398681f4568178de26311bf4
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-09 16:33:16 -0700

    Added Sidebar
    Added a sidebar with links to my LinkedIn, GitHub, a way to email me, and a way to download my resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -3,68 +3,75 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 const SocialLinks = () => {
-
-const links =  [
+  const links = [
     {
-        id:1,
-        child:(
-            <>
-            LinkedIn
-            <FaLinkedin size={30} />
-          </>
-        ),
-        href:"https://www.linkedin.com/in/jaredlee12345/",
-        style:'rounded-tr-md'
+      id: 1,
+      child: (
+        <>
+          LinkedIn
+          <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/jaredlee12345/",
+      style: "rounded-tr-md",
     },
     {
-        id:2,
-        child:(
-            <>
-            GitHub
-            <FaLinkedin size={30} />
-          </>
-        ),
-        href:"https://www.linkedin.com/in/jaredlee12345/",
-        style:'rounded-tr-md'
+      id: 2,
+      child: (
+        <>
+          GitHub
+          <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/untamedduck",
     },
     {
-        id:3,
-        child:(
-            <>
-            LinkedIn
-            <FaLinkedin size={30} />
-          </>
-        ),
-        href:"https://www.linkedin.com/in/jaredlee12345/",
-        style:'rounded-tr-md'
+      id: 3,
+      child: (
+        <>
+          Email
+          <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:jardlee123@gmail.com",
     },
     {
-        id:4,
-        child:(
-            <>
-            LinkedIn
-            <FaLinkedin size={30} />
-          </>
-        ),
-        href:"https://www.linkedin.com/in/jaredlee12345/",
-        style:'rounded-tr-md'
+      id: 4,
+      child: (
+        <>
+          Resume Download
+          <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
     },
-]
+  ];
 
   return (
     <div className="flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500 hover:rounded-md duration-300 hover:ml-[-10px]">
-          <a
-            href=""
-            className="flex justify-between items-center w-full text-white"
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500 hover:rounded-md duration-300 hover:ml-[-10px]" +
+              " " +
+              style
+            }
           >
-            <>
-              LinkedIn
-              <FaLinkedin size={30} />
-            </>
-          </a>
-        </li>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              terget="_blank"
+            >
+              {child}
+              <></>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
