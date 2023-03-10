e4f1c0fbb2e6b458898c743af38dceb366585156
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-10 10:49:00 -0700

    About Page
    Created the about page

--- portfolio/src/components/SocialLinks.jsx
+++ portfolio/src/components/SocialLinks.jsx
@@ -50,7 +50,7 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li
